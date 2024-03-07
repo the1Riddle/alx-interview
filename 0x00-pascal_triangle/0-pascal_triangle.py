3c22064c085ca007c1faa2248eb97eb11240303e
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-07 16:51:48 +0300

    pascal triangle: fixing pycodestyle issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,15 +13,20 @@ def pascal_triangle(n):
 
     for i in range(n):
         mylist = [1]
-        if i == 0:#if its the first digit then use
-            triangle.append(mylist)#mylist as the first digit
+        """if its the first digit then use"""
+        if i == 0:
+            """mylist as the first digit"""
+            triangle.append(mylist)
             continue
         else:
-            prev_row = triangle[-1]#get the last row
+            """get the last row"""
+            prev_row = triangle[-1]
             for j in range(1, i):
-                nex = prev_row[j -1] + prev_row[j]
+                nex = prev_row[j - 1] + prev_row[j]
                 mylist.append(nex)
-        mylist.append(1)#append 1 at the end of the list
-        triangle.append(mylist)#the first digit should always be mylist
+        """append 1 at the end of the list"""
+        mylist.append(1)
+        """the first digit should always be mylist"""
+        triangle.append(mylist)
 
     return triangle
