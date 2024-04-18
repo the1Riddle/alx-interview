0aad53dc6e149e71139918813e454d87b889fa5d
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-04-18 16:24:10 +0300

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Function caUnlockAll
+    """
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
